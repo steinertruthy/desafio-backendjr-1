9afb31334a8369d33d12aa416103a730a142dac8
steiner <steinerdstt7@gmail.com>
2023-06-02 14:38:27 -0300

    feat: add route get all users

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post("/users", [UserController::class, "store"]);
+Route::get("/users", [UserController::class, "index"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
